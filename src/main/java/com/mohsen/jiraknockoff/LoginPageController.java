39e0a5881c6d1a101e2d2cf4d335fc1f9a0b6e5a
Mohsen Mirzaei <mohsen.mirzaei2016@gmail.com>
2025-01-30 20:19:03 +0300

    update login page

---
+++ src/main/java/com/mohsen/jiraknockoff/LoginPageController.java
@@ -0,0 +1,66 @@
+package com.mohsen.jiraknockoff;
+
+import com.mohsen.jiraknockoff.db.DB;
+import com.mohsen.jiraknockoff.project.Project;
+import com.mohsen.jiraknockoff.project.Task;
+import com.mohsen.jiraknockoff.user.CurrentUser;
+import com.mohsen.jiraknockoff.user.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginPageController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label welcomeText;
+
+    DB database = DB.getInstance();
+
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        User user = database.findUserByUsername(username);
+        if (user != null) {
+            if (user.getPassword().equals(password)) {
+                welcomeText.setText("Welcome, " + username + "!");
+            } else {
+                welcomeText.setText("Incorrect password!");
+            }
+        } else {
+            database.addUser(user = new User(username, password));
+            welcomeText.setText("Account created!");
+        }
+
+        //////
+        user.addTeamMemberProject(new Project("Project 1", "dsk", user));
+        user.addTask(new Task("Task 1", Task.priority.LOW, Task.taskStatus.TODO, user));
+        //////
+
+        CurrentUser currentUser = CurrentUser.getInstance();
+        currentUser.setUser(user);
+        loadUserDashboard();
+    }
+
+    private void loadUserDashboard() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/mohsen/jiraknockoff/user-dashboard.fxml"));
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            stage.setScene(new Scene(root, 1000, 600));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
