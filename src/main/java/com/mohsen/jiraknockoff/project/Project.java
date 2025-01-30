af57050efaaa02ab63a623f7fb9c3e7f62a7d432
Mohsen Mirzaei <mohsen.mirzaei2016@gmail.com>
2025-01-30 20:18:30 +0300

    update project and user classes

---
+++ src/main/java/com/mohsen/jiraknockoff/project/Project.java
@@ -0,0 +1,76 @@
+package com.mohsen.jiraknockoff.project;
+
+import com.mohsen.jiraknockoff.user.User;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Project {
+    private String name;
+    private String description;
+    private User manager;
+    private List<User> teamMembers;
+    private List<Task> tasks;
+
+    public Project(String name, String description, User manager) {
+        this.name = name;
+        this.description = description;
+        this.manager = manager;
+        this.teamMembers = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getManager() {
+        return manager;
+    }
+
+    public void setManager(User manager) {
+        this.manager = manager;
+    }
+
+    public List<User> getTeamMembers() {
+        return teamMembers;
+    }
+
+    public void setTeamMembers(List<User> teamMembers) {
+        this.teamMembers = teamMembers;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task createTask(String title, Task.priority priority, Task.taskStatus status, User assignedTo) {
+        Task newTask = new Task(title, priority, status, assignedTo);
+        this.tasks.add(newTask);
+        return newTask;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/com/mohsen/jiraknockoff/user/User.java
@@ -0,0 +1,89 @@
+package com.mohsen.jiraknockoff.user;
+
+import com.mohsen.jiraknockoff.project.Project;
+import com.mohsen.jiraknockoff.project.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private String password;
+    private List<Project> teamMemberProjects;
+    private List<Project> managerProjects;
+    private List<Task> tasks;
+
+    public User(String username, String password, List<Project> teamMemberProjects, List<Project> managerProjects, List<Task> tasks) {
+        this.username = username;
+        this.password = password;
+        this.teamMemberProjects = teamMemberProjects;
+        this.managerProjects = managerProjects;
+        this.tasks = tasks;
+    }
+
+    public User(String username, String password) {
+        this(username, password, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Project> getTeamMemberProjects() {
+        return teamMemberProjects;
+    }
+
+    public void setTeamMemberProjects(List<Project> teamMemberProjects) {
+        this.teamMemberProjects = teamMemberProjects;
+    }
+
+    public List<Project> getManagerProjects() {
+        return managerProjects;
+    }
+
+    public void setManagerProjects(List<Project> managerProjects) {
+        this.managerProjects = managerProjects;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTeamMemberProject(Project project) {
+        this.teamMemberProjects.add(project);
+    }
+
+    public void addManagerProject(Project project) {
+        this.managerProjects.add(project);
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", teamMemberProjects=" + teamMemberProjects +
+                ", managerProjects=" + managerProjects +
+                ", tasks=" + tasks +
+                '}';
+    }
+}
