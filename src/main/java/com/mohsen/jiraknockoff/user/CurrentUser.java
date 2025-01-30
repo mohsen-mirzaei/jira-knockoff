6608f39b70f73ab5c6243b015eacc8cfd8808beb
Mohsen Mirzaei <mohsen.mirzaei2016@gmail.com>
2025-01-30 20:18:48 +0300

    add current user class as singleton

---
+++ src/main/java/com/mohsen/jiraknockoff/user/CurrentUser.java
@@ -0,0 +1,24 @@
+package com.mohsen.jiraknockoff.user;
+
+public class CurrentUser {
+    private static CurrentUser instance;
+    private User user;
+
+    private CurrentUser() {
+    }
+
+    public static synchronized CurrentUser getInstance() {
+        if (instance == null) {
+            instance = new CurrentUser();
+        }
+        return instance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
